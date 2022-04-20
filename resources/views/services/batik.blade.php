c190bf679fff635876b3e4c94c2a6c7e7e1b36a1
kevinandreanus <kevin@meestakes.com>
2022-04-20 09:59:36 +0700

    Fix Image wont load

--- resources/views/services/batik.blade.php
+++ resources/views/services/batik.blade.php
@@ -3,14 +3,14 @@
     <div class="container">
         <div class="text-center" style="margin-top: 100px">
             <h3 style="color: #806e4f !important;">SERVICES</h3>
-            <img class="mt-3" src="{{ asset('images/services/batik.PNG') }}" alt="">
+            <img class="mt-3" src="{{ asset('images/services/batik.png') }}" alt="">
 
             <h3 class="mt-5" style="color: #806e4f !important;">BATIK</h3>
             <p class="lookBookP">We provide tailoring for Batik as batik is part of our culture
                 Changsan: We prodivde Sateen Cheongsam for chinese tradition of Sangjit ceremony</p>
-            <img class="mt-5" src="{{ asset('images/services/pic1.PNG') }}" alt="">
-            <img class="mt-3" src="{{ asset('images/services/pic2.PNG') }}" alt="">
-            <img class="mt-3" src="{{ asset('images/services/pic3.PNG') }}" alt="">
+            <img class="mt-5" src="{{ asset('images/services/pic1.png') }}" alt="">
+            <img class="mt-3" src="{{ asset('images/services/pic2.png') }}" alt="">
+            <img class="mt-3" src="{{ asset('images/services/pic3.png') }}" alt="">
             <br>
         </div>
         <div class="text-center mt-5">
--- resources/views/services/cheongsam.blade.php
+++ resources/views/services/cheongsam.blade.php
@@ -1,21 +1,19 @@
 @extends('template.template')
 @section('content')
-    
-
-<div class="container">
+    <div class="container">
         <div class="text-center" style="margin-top: 100px">
             <h3 style="color: #806e4f !important;">SERVICES</h3>
-            <img class="mt-3" src="{{ asset('images/services/cheongsam.PNG') }}" alt="">
+            <img class="mt-3" src="{{ asset('images/services/cheongsam.png') }}" alt="">
 
             <h3 class="mt-5" style="color: #806e4f !important;">CHEONGSAM</h3>
-            <p class="lookBookP">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+            <p class="lookBookP">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
                 sed diam nonummy nibh euismod
                 tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
                 nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
                 Duis autem vel eum.</p>
-            <img class="mt-5" src="{{ asset('images/services/pic1.PNG') }}" alt="">
-            <img class="mt-3" src="{{ asset('images/services/pic2.PNG') }}" alt="">
-            <img class="mt-3" src="{{ asset('images/services/pic3.PNG') }}" alt="">
+            <img class="mt-5" src="{{ asset('images/services/pic1.png') }}" alt="">
+            <img class="mt-3" src="{{ asset('images/services/pic2.png') }}" alt="">
+            <img class="mt-3" src="{{ asset('images/services/pic3.png') }}" alt="">
             <br>
         </div>
         <div class="text-center mt-5">
