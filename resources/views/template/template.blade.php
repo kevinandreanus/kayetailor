3f2d78926c79f4f5c0d4776c35e79b8299870255
kevinandreanus <kevin@meestakes.com>
2022-09-06 20:29:43 +0700

    Revisi

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -6,13 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Kaye Brother's Tailor</title>
-
+    <link rel="icon" href="{{ asset('faviconn.ico') }}" type="image/x-icon" />
     <link rel="stylesheet" href="{{ asset('css/main2.css') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
         rel="stylesheet">
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" />
     <link rel="stylesheet" href="{{ asset('css/custstyle.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
